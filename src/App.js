e650469492a90d72027f93393920eca83ac99524
Chaouch Hamza <h.chaouch@openbee.com>
2019-06-28 10:21:27 +0200

    lifting the state up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Liste from "./components/liste";
 import Navbar from "./components/Navbar";
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <div className="container">
-        <Liste />
-      </div>
-    </React.Fragment>
-  );
-}
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 100 },
+      { id: 2, value: 50000 },
+      { id: 3, value: 0 },
+      { id: 4, value: 50 },
+      { id: 5, value: 1003 }
+    ]
+  };
+  handleIncrement = idcounter => {
+    // console.log(idcounter);
+    const counters = [...this.state.counters];
+    counters[idcounter - 1].value++;
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(i => {
+      i.value = 0;
+      return i;
+    });
+    this.setState({
+      counters
+    });
+  };
+  handleDelete = counterId => {
+    //console.log("event handler", counterId);
+    const counters = this.state.counters.filter(item => item.id !== counterId);
+    this.setState({ counters: counters });
+  };
 
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Liste
+            counters={this.state.counters}
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 export default App;
--- src/components/liste.js
+++ src/components/liste.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class liste extends Component {
-  state = {
+  /*   state = {
     counters: [
       { id: 1, value: 100 },
       { id: 2, value: 50000 },
@@ -30,22 +30,22 @@ class liste extends Component {
     //console.log("event handler", counterId);
     const counters = this.state.counters.filter(item => item.id !== counterId);
     this.setState({ counters: counters });
-  };
+  }; */
 
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(item => (
+        {this.props.counters.map(item => (
           <Counter
-            onDelete={this.handleDelete}
+            onDelete={this.props.onDelete}
             key={item.id}
-            onIncrement={this.handleIncrement}
+            onIncrement={this.props.onIncrement}
             value={item.value}
             id={item.id}
           />
